7b3dbab1b57c18005a02fb6c5339723084de00d3
Raghav Sharma <Sharmaraghavv382@gmail.com>
2023-01-11 11:25:04 +1000

    Added Home and signup route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
+import Home from "./routes/home/home.component";
 import SignUpPage from "./routes/sign-up/sign-up.component";
 function App() {
   return (
-    <div className="App">
-      Hello World!
-      <SignUpPage />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </>
   );
 }
 
